d37d773b87567f4ded6c17c8ecf571be04144eb9
Thomas Rothenbacher <thomas.rothenbacher@check24.de>
2021-05-02 19:16:44 +0200

    implement swiping

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Alert } from 'react-native';
+import axios from 'axios';
 import Constants from 'expo-constants';
 import TopBar from './components/TopBar';
 import SwipeableImage from './components/SwipeableImage';
 import BottomBar from './components/BottomBar';
-import axios from 'axios';
+import Swipes from './components/Swipes';
+
+
 
 export default function App() {
 
   const [users, setUsers] = useState([]);
-  const [currentIndex, setCurrentUser] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
   async function fetchUsers() {
     try {
       const { data } = await axios.get('https://randomuser.me/api/?gender=female&results=50');
@@ -25,13 +28,37 @@ export default function App() {
     fetchUsers()
   }, []);
 
+  function handleLike() {
+    console.log('like');
+    nextUser();
+  }
+
+  function handlePass() {
+    console.log('pass');
+    nextUser();
+  }
+
+  function nextUser() {
+    const nextIndex = users.length - 2 === currentIndex ? 0 : currentIndex + 1;
+    setCurrentIndex(nextIndex);
+  }
+
   return (
     <View style={styles.container}>
       <TopBar />
       <View style={styles.swipes}>
-        {users.length > 1 && (
-          <SwipeableImage user={users[currentIndex]} />
-        )}
+        {users.length > 1 && users.map(
+          (u, i) =>
+            currentIndex === i && (
+              <Swipes
+                key={i}
+                currentIndex={currentIndex}
+                users={users}
+                handleLike={handleLike}
+                handlePass={handlePass}
+              >
+              </Swipes>
+            ))}
       </View>
       <BottomBar />
     </View>
---
+++ components/Swipes.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { RectButton } from 'react-native-gesture-handler';
+import SwipeableImage from './SwipeableImage';
+
+export default function Swipes({ users, currentIndex, handleLike, handlePass }) {
+
+    const renderLeftActions = () => {
+        return (
+            <RectButton style={styles.container}>
+                <SwipeableImage user={users[currentIndex + 1]}></SwipeableImage>
+            </RectButton>
+        );
+    }
+
+    const renderRightActions = () => {
+        return (
+            <RectButton style={styles.container}>
+                <SwipeableImage user={users[currentIndex + 1]}></SwipeableImage>
+            </RectButton>
+        );
+    }
+
+    return (
+        <Swipeable
+            friction={2}
+            leftThreshold={40}
+            rightThreshold={40}
+            renderLeftActions={renderLeftActions}
+            renderRightActions={renderRightActions}
+            onSwipeableLeftOpen={handleLike}
+            onSwipeableRightOpen={handlePass}
+        >
+            <SwipeableImage user={users[currentIndex]} />
+        </Swipeable>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
